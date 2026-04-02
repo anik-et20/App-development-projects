6852bbe55c2e90e5c226d9f62920e370d8ccf25c
Aniket <aniketvaish15@gmail.com>
2026-04-02 23:09:14 +0530

    Sensor app pushed

---
+++ Sensorapp/app/src/main/java/com/example/sensorapp/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.sensorapp;
+import android.hardware.Sensor;
+import android.annotation.SuppressLint;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView accelText, lightText, proximityText;
+    private SensorManager sensorManager;
+    private Sensor accelerometer, lightSensor, proximitySensor;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        accelText = findViewById(R.id.accelText);
+        lightText = findViewById(R.id.lightText);
+        proximityText = findViewById(R.id.proximityText);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            accelText.setText("Accelerometer:\nX: " + x + "\nY: " + y + "\nZ: " + z);
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            float light = event.values[0];
+            lightText.setText("Light: " + light + " lx");
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            float proximity = event.values[0];
+            proximityText.setText("Proximity: " + proximity + " cm");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (lightSensor != null) {
+            sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (proximitySensor != null) {
+            sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+}
